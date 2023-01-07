8e22490a33f06ad5bfd17e6a6d8a4a5844d6b997
Steven Garberg <steven.garberg.jr@gmail.com>
2023-01-07 15:09:23 -0500

    Added scripts

---
+++ Unity/Assets/Scripts/Controllers/EventController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LD52.Controllers
+{
+    public static class EventController
+    {
+        public static event Action OnGameStarted;
+        public static void GameStarted() => OnGameStarted?.Invoke();
+        
+        public static event Action OnGameOver;
+        public static void GameOver() => OnGameOver?.Invoke();
+    }
+}
