ba05c513e9542bebe55beb52330de75fb1fbb655
Trevor <reevest2@gmail.com>
2023-01-07 21:48:37 -0500

    Squashed commit of the following:
    commit 580803a7cbdd2acc0b8eddefecc06b259e02a3e3
    Author: Trevor <reevest2@gmail.com>
    Date:   Sat Jan 7 21:44:22 2023 -0500
    
        snap back to original position

--- Unity/Assets/Scripts/Behaviors/CropDragger.cs
+++ Unity/Assets/Scripts/Behaviors/CropDragger.cs
@@ -7,15 +7,18 @@ namespace LD52
     public class CropDragger : MonoBehaviour
     {
         private bool isDragging;
+        private Vector2 initialPosition;
 
         public void OnMouseDown()
         {
             isDragging = true;
+            initialPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         }
 
         public void OnMouseUp()
         {
             isDragging = false;
+            transform.Translate(initialPosition);
         }
 
         // Update is called once per frame
